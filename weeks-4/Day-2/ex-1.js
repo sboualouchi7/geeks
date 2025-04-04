7fd52e854df84980ca9ad9ab7ac2f6690fe3f7f5
sboualouchi7 <salmanboualouchi@gmail.com>
2025-04-04 21:30:44 +0000

    to be removed

--- weeks-4/Day-2/ex-1.js
+++ weeks-4/Day-2/ex-1.js
@@ -1,12 +1,4 @@
 const colors = ["Blue", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow"];
 
 const color = () => {
-    colors.forEach((color, index) => {
-     console.log(`The #${index+1} choice is ${color}`);
-    });
-    if (colors.find(color => color === "Violet")) {
-     return "Yes";
-    }
-     return "No";
- }
- console.log(color());
+    colors.fjdqnhjnsjn
